79c48f7af2952c63fa130e45a12bfb3bdcb5b012
your_name <neymargonzayo@gmail.com>
2026-03-16 08:14:28 -0300

    first commit

---
+++ src/Livro.java
@@ -0,0 +1,12 @@
+import java.time.LocalTime;
+
+public class Livro {
+    String titulo;
+    String autor;
+    LocalTime duracao;
+    String resumo;
+     double preco;
+     String editora;
+    String narrador;
+    String versao;
+}
