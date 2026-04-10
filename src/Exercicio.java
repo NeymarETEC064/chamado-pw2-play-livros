163637d532103f1ec578c59772679f79f93b81aa
Franz-Neymar <nome1-nome2@example.com>
2026-04-10 17:27:54 -0300

    Alteracao de classes relacionadas ao exercício físico

---
+++ src/Exercicio.java
@@ -0,0 +1,10 @@
+import br.com.etechoracio.playlivros.enums.DificuldadeEnum;
+
+public class Exercicio {
+    public String nome;
+    public String grupoMuscular;
+    public int repeticoes;
+    public int series;
+    public double carga;
+    public DificuldadeEnum dificulade;
+}
