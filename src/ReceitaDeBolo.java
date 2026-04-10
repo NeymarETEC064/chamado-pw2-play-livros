44e279c18bb133f76929f6ae7ce4a0268e5dada6
Franz-Neymar <nome1-nome2@example.com>
2026-04-10 17:10:54 -0300

    Alteracao de classes relacionadas a receita

---
+++ src/ReceitaDeBolo.java
@@ -0,0 +1,12 @@
+import br.com.etechoracio.playlivros.model.Avaliacao;
+import br.com.etechoracio.playlivros.model.Usuario;
+
+import java.time.LocalTime;
+
+public class ReceitaDeBolo {
+    String nome;
+    LocalTime tempoPreparo;
+    int porcoes;
+    public Avaliacao avaliacao;
+    public Usuario confeiteiro;
+}
